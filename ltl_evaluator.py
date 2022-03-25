f6e5011399543630408db6a5611b14239fc2f54d
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-25 18:41:02 +0000

    KISS: created class OperatorsSolver to avoid passing different arguments to every function

--- ltl_evaluator.py
+++ ltl_evaluator.py
@@ -1,24 +1,25 @@
 from parsing.token import TokenType
-from operators import *
+from operators import OperatorsSolver
 
 def evaluate(operators, states, loop_size):
     computed_rows = []
+    solver = OperatorsSolver(states, loop_size, computed_rows)
     for op in operators:
-        computed_rows.append(__solve_operator(op, computed_rows, states, loop_size))
+        computed_rows.append(__solve_operator(op, solver))
     last_row = computed_rows[-1].boolean_array
     return all(b == True for b in last_row)
     
-def __solve_operator(op, computed_rows, states, loop_size):
+def __solve_operator(op, solver):
     match op.oper():
-        case TokenType.ATOM: return solve_atom(op, states)
-        case TokenType.NOT: return solve_not(op, computed_rows)
-        case TokenType.AND: return solve_and(op, computed_rows)
-        case TokenType.OR: return solve_or(op, computed_rows)
-        case TokenType.IMPL: return solve_impl(op, computed_rows)
-        case TokenType.U: return solve_U(op, computed_rows, loop_size)
-        case TokenType.X: return solve_X(op, computed_rows, loop_size)
-        case TokenType.F: return solve_F(op, computed_rows, loop_size)
-        case TokenType.R: return solve_R(op, computed_rows, loop_size)
-        case TokenType.G: return solve_G(op, computed_rows, loop_size)
-        case TokenType.W: return solve_W(op, computed_rows, loop_size)
+        case TokenType.ATOM: return solver.solve_atom(op)
+        case TokenType.NOT: return solver.solve_not(op)
+        case TokenType.AND: return solver.solve_and(op)
+        case TokenType.OR: return solver.solve_or(op)
+        case TokenType.IMPL: return solver.solve_impl(op)
+        case TokenType.U: return solver.solve_U(op)
+        case TokenType.X: return solver.solve_X(op)
+        case TokenType.F: return solver.solve_F(op)
+        case TokenType.R: return solver.solve_R(op)
+        case TokenType.G: return solver.solve_G(op)
+        case TokenType.W: return solver.solve_W(op)
         case _: raise Exception("No available implementation for operator " + op)       
--- operators.py
+++ operators.py
@@ -2,76 +2,83 @@ from Row import Row
 from base_operators import *
 from util import *
 
-# ---- 0 arity operators ----
+class OperatorsSolver:
 
-def solve_atom(op, states):
-    bool_row = []
-    for s in states:
-        bool_row.append(op in s.labeling)
-    return Row(op, bool_row)
-    
-# ---- 1 arity operators ----
+    def __init__(self, states, loop_size, computed_rows):
+        self.states = states
+        self.loop_size = loop_size
+        self.computed_rows = computed_rows
 
-def solve_not(op, computed_rows):
-    child = get_child_boolean_array(op, computed_rows)
-    # base operator
-    return Row(op, NOT_operator(child))
+    # ---- 0 arity operators ----
 
-def solve_X(op, computed_rows, loop_size):
-    child = get_child_boolean_array(op, computed_rows)
-    # base operator
-    return Row(op, X_operator(child, loop_size))
+    def solve_atom(self, op):
+        bool_row = []
+        for s in self.states:
+            bool_row.append(op in s.labeling)
+        return Row(op, bool_row)
+        
+    # ---- 1 arity operators ----
 
-def solve_F(op, computed_rows, loop_size):
-    child = get_child_boolean_array(op, computed_rows)
-    true_arr = create_array_with_value(len(child), True)
-    # F(a) == true U a
-    return Row(op, U_operator(true_arr, child, loop_size))
+    def solve_not(self, op):
+        child = get_child_boolean_array(op, self.computed_rows)
+        # base operator
+        return Row(op, NOT_operator(child))
 
-def solve_G(op, computed_rows, loop_size):
-    child = get_child_boolean_array(op, computed_rows)
-    true_arr = create_array_with_value(len(child), True)
-    # G(a) == false R a == !F(!a) == !(true U !a)
-    U = U_operator(true_arr, NOT_operator(child), loop_size)
-    return Row(op, NOT_operator(U))
+    def solve_X(self, op):
+        child = get_child_boolean_array(op, self.computed_rows)
+        # base operator
+        return Row(op, X_operator(child, self.loop_size))
 
-# ---- 2 arity operators ----
+    def solve_F(self, op):
+        child = get_child_boolean_array(op, self.computed_rows)
+        true_arr = create_array_with_value(len(child), True)
+        # F(a) == true U a
+        return Row(op, U_operator(true_arr, child, self.loop_size))
 
-def solve_U(op, computed_rows, loop_size):
-    left_child = get_left_child_boolean_array(op, computed_rows)
-    right_child = get_right_child_boolean_array(op, computed_rows)
-    # base operator
-    return Row(op, U_operator(left_child, right_child, loop_size))
+    def solve_G(self, op):
+        child = get_child_boolean_array(op, self.computed_rows)
+        true_arr = create_array_with_value(len(child), True)
+        # G(a) == false R a == !F(!a) == !(true U !a)
+        U = U_operator(true_arr, NOT_operator(child), self.loop_size)
+        return Row(op, NOT_operator(U))
 
-def solve_or(op, computed_rows):
-    left_child = get_left_child_boolean_array(op, computed_rows)
-    right_child = get_right_child_boolean_array(op, computed_rows)
-    # base operator
-    return Row(op, OR_operator(left_child, right_child))
+    # ---- 2 arity operators ----
 
-def solve_impl(op, computed_rows):
-    left_child = get_left_child_boolean_array(op, computed_rows)
-    right_child = get_right_child_boolean_array(op, computed_rows)
-    # a -> b == !a || b
-    return Row(op, OR_operator(NOT_operator(left_child), right_child))
+    def solve_U(self, op):
+        a = get_left_child_boolean_array(op, self.computed_rows)
+        b = get_right_child_boolean_array(op, self.computed_rows)
+        # base operator
+        return Row(op, U_operator(a, b, self.loop_size))
 
-def solve_and(op, computed_rows):
-    left_child = get_left_child_boolean_array(op, computed_rows)
-    right_child = get_right_child_boolean_array(op, computed_rows)
-    # a && b == !(!a || !b)
-    return Row(op, NOT_operator(OR_operator(NOT_operator(left_child), NOT_operator(right_child))))
+    def solve_or(self, op):
+        a = get_left_child_boolean_array(op, self.computed_rows)
+        b = get_right_child_boolean_array(op, self.computed_rows)
+        # base operator
+        return Row(op, OR_operator(a, b))
 
-def solve_R(op, computed_rows, loop_size):
-    left_child = get_left_child_boolean_array(op, computed_rows)
-    right_child = get_right_child_boolean_array(op, computed_rows)
-    # a R b == !(!a U !b)
-    U = U_operator(NOT_operator(left_child), NOT_operator(right_child), loop_size)
-    return Row(op, NOT_operator(U))
+    def solve_impl(self, op):
+        a = get_left_child_boolean_array(op, self.computed_rows)
+        b = get_right_child_boolean_array(op, self.computed_rows)
+        # a -> b == !a || b
+        return Row(op, OR_operator(NOT_operator(a), b))
 
-def solve_W(op, computed_rows, loop_size):
-    a = get_left_child_boolean_array(op, computed_rows)
-    b = get_right_child_boolean_array(op, computed_rows)
-    # a W b == (a U b) || G(a) == a U (b || G(a)) == b R (b || a) == !(!b U !(b || a))
-    OR = OR_operator(b, a)
-    U = U_operator(NOT_operator(b), NOT_operator(OR), loop_size)
-    return Row(op, NOT_operator(U))
+    def solve_and(self, op):
+        a = get_left_child_boolean_array(op, self.computed_rows)
+        b = get_right_child_boolean_array(op, self.computed_rows)
+        # a && b == !(!a || !b)
+        return Row(op, NOT_operator(OR_operator(NOT_operator(a), NOT_operator(b))))
+
+    def solve_R(self, op):
+        a = get_left_child_boolean_array(op, self.computed_rows)
+        b = get_right_child_boolean_array(op, self.computed_rows)
+        # a R b == !(!a U !b)
+        U = U_operator(NOT_operator(a), NOT_operator(b), self.loop_size)
+        return Row(op, NOT_operator(U))
+
+    def solve_W(self, op):
+        a = get_left_child_boolean_array(op, self.computed_rows)
+        b = get_right_child_boolean_array(op, self.computed_rows)
+        # a W b == (a U b) || G(a) == a U (b || G(a)) == b R (b || a) == !(!b U !(b || a))
+        OR = OR_operator(b, a)
+        U = U_operator(NOT_operator(b), NOT_operator(OR), self.loop_size)
+        return Row(op, NOT_operator(U))
