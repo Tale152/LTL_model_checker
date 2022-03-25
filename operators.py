a0ae209fe5ce35396432142e6c0deb54a1e924f8
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-25 16:28:17 +0000

    added some comments to operators.py

--- operators.py
+++ operators.py
@@ -1,6 +1,7 @@
 from Row import Row
 from base_operators import U_operator, X_operator, OR_operator, NOT_operator
 
+# ---- utility functions ----
 def find_row(op, computed_rows):
     for r in computed_rows:
         if r.operator == op:
@@ -12,34 +13,8 @@ def get_left_child(op, computed_rows):
 
 def get_right_child(op, computed_rows):
     return find_row(op.children()[1], computed_rows).boolean_array
-    
-def solve_X(op, computed_rows, loop_size):
-    child = get_left_child(op, computed_rows)
-    return Row(op, X_operator(child, loop_size))
 
-def solve_U(op, computed_rows, loop_size):
-    left_child = get_left_child(op, computed_rows)
-    right_child = get_right_child(op, computed_rows)
-    return Row(op, U_operator(left_child, right_child, loop_size))
-
-def solve_impl(op, computed_rows):
-    left_child = get_left_child(op, computed_rows)
-    right_child = get_right_child(op, computed_rows)
-    return Row(op, OR_operator(NOT_operator(left_child), right_child))
-
-def solve_or(op, computed_rows):
-    left_child = get_left_child(op, computed_rows)
-    right_child = get_right_child(op, computed_rows)
-    return Row(op, OR_operator(left_child, right_child))
-
-def solve_and(op, computed_rows):
-    left_child = get_left_child(op, computed_rows)
-    right_child = get_right_child(op, computed_rows)
-    return Row(op, NOT_operator(OR_operator(NOT_operator(left_child), NOT_operator(right_child))))
-
-def solve_not(op, computed_rows):
-    child = get_left_child(op, computed_rows)
-    return Row(op, NOT_operator(child))
+# ---- 0 arity operators ----
 
 def solve_atom(op, states):
     prop = ".." + str(op.value)
@@ -51,3 +26,41 @@ def solve_atom(op, states):
                 found = True
         bool_row.append(found)
     return Row(op, bool_row)
+    
+# ---- 1 arity operators ----
+
+def solve_X(op, computed_rows, loop_size):
+    child = get_left_child(op, computed_rows)
+    # base operator
+    return Row(op, X_operator(child, loop_size))
+
+def solve_not(op, computed_rows):
+    child = get_left_child(op, computed_rows)
+    # base operator
+    return Row(op, NOT_operator(child))
+
+# ---- 2 arity operators ----
+
+def solve_U(op, computed_rows, loop_size):
+    left_child = get_left_child(op, computed_rows)
+    right_child = get_right_child(op, computed_rows)
+    # base operator
+    return Row(op, U_operator(left_child, right_child, loop_size))
+
+def solve_or(op, computed_rows):
+    left_child = get_left_child(op, computed_rows)
+    right_child = get_right_child(op, computed_rows)
+    # base operator
+    return Row(op, OR_operator(left_child, right_child))
+
+def solve_impl(op, computed_rows):
+    left_child = get_left_child(op, computed_rows)
+    right_child = get_right_child(op, computed_rows)
+    # a -> b == !a || b
+    return Row(op, OR_operator(NOT_operator(left_child), right_child))
+
+def solve_and(op, computed_rows):
+    left_child = get_left_child(op, computed_rows)
+    right_child = get_right_child(op, computed_rows)
+    # a && b == !(!a || !b)
+    return Row(op, NOT_operator(OR_operator(NOT_operator(left_child), NOT_operator(right_child))))
