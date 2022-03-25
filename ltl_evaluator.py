0a6c879e92c59781f6b6f7eab45fef77dfdb89fd
Tale152 <alessandro.talmi@studio.unibo.it>
2022-03-25 17:33:00 +0000

    KISS: refactored ltl_evaluator.py

--- ltl_evaluator.py
+++ ltl_evaluator.py
@@ -1,42 +1,24 @@
-from Row import Row
 from parsing.token import TokenType
-from operators import solve_atom, solve_not, solve_and, solve_or, solve_impl, solve_U, solve_X, solve_F, solve_R, solve_G, solve_W
-
-def solve_operator(op, computed_rows, states, loop_size):
-    token = op.oper()
-    if token == TokenType.ATOM:
-        return solve_atom(op, states)
-    elif token == TokenType.NOT:
-        return solve_not(op, computed_rows)
-    elif token == TokenType.AND:
-        return solve_and(op, computed_rows)
-    elif token == TokenType.OR:
-        return solve_or(op, computed_rows)
-    elif token == TokenType.IMPL:
-        return solve_impl(op, computed_rows)
-    elif token == TokenType.U:
-        return solve_U(op, computed_rows, loop_size)
-    elif token == TokenType.X:
-        return solve_X(op, computed_rows, loop_size)
-    elif token == TokenType.F:
-        return solve_F(op, computed_rows, loop_size)
-    elif token == TokenType.R:
-        return solve_R(op, computed_rows, loop_size)
-    elif token == TokenType.G:
-        return solve_G(op, computed_rows, loop_size)
-    elif token == TokenType.W:
-        return solve_W(op, computed_rows, loop_size)
-    else:
-        raise Exception("No available implementation for token " + token)
-
-def define_result(last_boolean_row):
-    for r in last_boolean_row:
-        if r == False:
-            return False
-    return True
+from operators import *
 
 def evaluate(operators, states, loop_size):
     computed_rows = []
     for op in operators:
-        computed_rows.append(solve_operator(op, computed_rows, states, loop_size))
-    return define_result(computed_rows[-1].boolean_array)
+        computed_rows.append(__solve_operator(op, computed_rows, states, loop_size))
+    last_row = computed_rows[-1].boolean_array
+    return all(b == True for b in last_row)
+    
+def __solve_operator(op, computed_rows, states, loop_size):
+    match op.oper():
+        case TokenType.ATOM: return solve_atom(op, states)
+        case TokenType.NOT: return solve_not(op, computed_rows)
+        case TokenType.AND: return solve_and(op, computed_rows)
+        case TokenType.OR: return solve_or(op, computed_rows)
+        case TokenType.IMPL: return solve_impl(op, computed_rows)
+        case TokenType.U: return solve_U(op, computed_rows, loop_size)
+        case TokenType.X: return solve_X(op, computed_rows, loop_size)
+        case TokenType.F: return solve_F(op, computed_rows, loop_size)
+        case TokenType.R: return solve_R(op, computed_rows, loop_size)
+        case TokenType.G: return solve_G(op, computed_rows, loop_size)
+        case TokenType.W: return solve_W(op, computed_rows, loop_size)
+        case _: raise Exception("No available implementation for operator " + op)       
--- test.py
+++ test.py
@@ -2,12 +2,7 @@ from interface import parse, getpath
 from rows_extractor import create_rows_array
 from ltl_evaluator import evaluate
 
-parsed = parse("(employee_left || employee_trans) W affea")
+parsed = parse("(employee_left || employee_trans || employee_right)")
 
 path = getpath("./paths/path0.txt")
-
-#print(len(create_rows_array(parsed)))
-#print(path.path[0].labeling[0])
 print(evaluate(create_rows_array(parsed), path.path, path.loop))
-print("WORKING")
-
